fe3b8c45e37d40ec8e3b57aeaaaa93af1a8d6971
Anibal Ladera <46658907+ajladera@users.noreply.github.com>
2021-09-26 08:15:34 -0400

    Add files via upload

---
+++ python-filezilla.py
@@ -0,0 +1,16 @@
+import glob
+from ftplib import FTP
+
+
+ftp = FTP()
+ftp.connect('host-ftp', port) #example --->>> ftp.connect('ftp-files.integracion.io', 2124)
+ftp.login(user='user', passwd='passwd')
+ftp.retrlines('LIST')
+ftp.cwd('/directory')
+
+filenames = ftp.nlst()
+for filename in filenames:
+    with open( filename, 'wb' ) as file :
+        ftp.retrbinary('RETR %s' % filename, file.write)
+        file.close()
+ftp.quit()
